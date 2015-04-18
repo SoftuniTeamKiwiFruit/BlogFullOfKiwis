3095bbd369c876eb3ed2b608bce723a7324657e7
BarishYumerov <barish.yumerov@abv.bg>
2015-04-18 13:23:22 +0300

    fixing
    test

--- BlogFullOfKiwis/scripts/View-Factory.js
+++ BlogFullOfKiwis/scripts/View-Factory.js
@@ -6,6 +6,22 @@ var app = app || {};
         this.model = model;
     }
 
+    ViewFactory.prototype.loadPosts = function(){
+        this.model.getAllPosts(function(data){
+            console.log(data);
+            for(var i = 0; i < data.results.length; i++){
+                console.log(i);
+                var post = $("<article/>").attr('id', data.results[i].objectId);
+                post.append("<h3>" + data.results[i].title +"</h3>")
+                    .append("<p>Content: " + data.results[i].content +"</p>");
+                console.log($('#sideBar'));
+                console.log(post);
+                $('#sideBar').append(post);
+            }
+        },
+        function(err){console.log(err.responseText)});
+    };
+
 
 
     app.module.ViewFactory = ViewFactory;
