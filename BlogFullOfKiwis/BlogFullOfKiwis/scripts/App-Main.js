cfb032bb9eb97fff30a0761e27c3cbf5369b3fc6
asyamincheva <asya.m.mincheva@gmail.com>
2015-04-18 12:31:28 +0300

    moved files

--- BlogFullOfKiwis/BlogFullOfKiwis/scripts/App-Main.js
+++
@@ -1,6 +0,0 @@
-var app = app || {};
-
-(function(){
-    var post = app.module.post('https://api.parse.com/1/classes/');
-    var comment = app.module.comment('https://api.parse.com/1/classes/');
-}());
