b6f470dc40300b83f28e9a655cdcaf176bdc3658
asyamincheva <asya.m.mincheva@gmail.com>
2015-04-18 12:20:47 +0300

    added sln file

---
+++ BlogFullOfKiwis/BlogFullOfKiwis/scripts/MakeRequest.js
@@ -0,0 +1,18 @@
+var app = app || {};
+
+(function(){
+    var MakeRequest = function(method, baseUrl, data, success, error){
+        $.ajax({
+            method: method,
+            headers:{
+                'X-Parse-Application-Id' : '5k4RvSznrQPfvnd87PjLJNfIDgIHtJyN0F46Vg75',
+                'X-Parse-REST-API-Key' : 'LXyzL8F0gvdHLwMhGkhE6BJIQb9ilAQQBJBwRLyE'
+            },
+            url: baseUrl,
+            data: data
+        }).success(success).error(error)
+    };
+
+    app.makeRequest = MakeRequest;
+    app.module = {};
+}());
---
+++ BlogFullOfKiwis/BlogFullOfKiwis/scripts/Post-Model.js
@@ -0,0 +1,26 @@
+var app = app || {};
+
+(function(){
+
+    var Post = function(url){
+        this.url = url;
+    };
+
+    Post.prototype.getAllPosts = function(success, error){
+        app.makeRequest("GET", this.url + 'Post/', null, success, error);
+    };
+
+    Post.prototype.postPost = function(data, success, error){
+        app.makeRequest("POST", this.url + 'Post/', data, success, error);
+    };
+
+    Post.prototype.editPost = function(id, data, success, error){
+        app.makeRequest("PUT", this.url + 'Post/' + id, data, success, error);
+    };
+
+    Post.prototype.deletePost = function(id, success, error){
+        app.makeRequest("DELETE", this.url + 'Post/' + id, success, error);
+    };
+
+    app.module.post = Post;
+}());
