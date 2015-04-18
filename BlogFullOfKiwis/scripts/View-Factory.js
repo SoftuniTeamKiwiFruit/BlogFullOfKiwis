1a43440efa58f997dcf66b1b89bd0be1b52ffff2
BarishYumerov <barish.yumerov@abv.bg>
2015-04-18 14:15:49 +0300

    fixing new factory

--- BlogFullOfKiwis/scripts/View-Factory.js
+++ BlogFullOfKiwis/scripts/View-Factory.js
@@ -8,14 +8,10 @@ var app = app || {};
 
     ViewFactory.prototype.loadPosts = function(){
         this.model.getAllPosts(function(data){
-            console.log(data);
             for(var i = 0; i < data.results.length; i++){
-                console.log(i);
                 var post = $("<article/>").attr('id', data.results[i].objectId);
                 post.append("<h3>" + data.results[i].title +"</h3>")
                     .append("<p>Content: " + data.results[i].content +"</p>");
-                console.log($('#sideBar'));
-                console.log(post);
                 $('#sideBar').append(post);
             }
         },
